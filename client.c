fc6ac2dde6abf913efc5233218649a10d8a7b335
susmit <susmit@cs.colostate.edu>
2013-03-08 04:42:42 -0700

    initial workin version

--- client.c
+++ client.c
@@ -9,46 +9,64 @@
 #include <ccn/uri.h>
 #include <ccn/keystore.h>
 #include <ccn/signing.h>
+#include <sys/time.h>
 
 #define CLI_PROGRAM "cbr_client"
 #define DEBUG
 
+char *URI;
 
 enum ccn_upcall_res incoming_interest(struct ccn_closure *selfp,
-                                      enum ccn_upcall_kind kind, struct ccn_upcall_info *info)
-{
+                                      enum ccn_upcall_kind kind, struct ccn_upcall_info *info) {
     //this is the callback function, all interest matching ccnx:/trace
     //will come here, handle them as appropriate
     int res = 0;
     const unsigned char *ptr;
     size_t length;
-    int i;
 
-    
-    char *delims = "~";
-    int hop = 0;
-    char *result = NULL;
+
     //switch on type of event
-    switch (kind)
-    {
+    switch (kind) {
     case CCN_UPCALL_FINAL:
         free(selfp);
         return CCN_UPCALL_RESULT_OK;
 
     case CCN_UPCALL_CONTENT:
-
         //get the content from packet
         res = ccn_content_get_value(info->content_ccnb, info->pco->offset[CCN_PCO_E], info->pco, &ptr, &length);
-        if (res < 0)
-        {
+        if (res < 0) {
             printf("Can not get value from content. res: %d", res);
             exit(1);
         }
-        
-        printf("Size received  %s %lu\n", ptr, info->pco->offset[CCN_PCO_E]);
- 
+        printf("Size received asd %d\n", info->pco->offset[CCN_PCO_E]);
+//        printf("Content %s\n", ptr);
+        while(1) {
+            char *new_URI;
+
+            char *rand_str = strrchr(URI, '/');
+            *rand_str  = '\0';
+            new_URI = (char *) calloc(strlen(URI) + 20 + 1, sizeof(char)); //assuming rand() length 20
+            sprintf(new_URI, "%s/%d", URI, rand());
+            printf("URI %s \n", new_URI);
+            URI = new_URI;
+
+            struct ccn_charbuf *ccnb = ccn_charbuf_create();
+            res = ccn_name_from_uri(ccnb, URI);
+            struct ccn_closure *incoming;
+            incoming = calloc(1, sizeof(*incoming));
+            incoming->p = incoming_interest;
+
+            res = ccn_express_interest(info->h, ccnb, incoming, NULL);
+            res = ccn_content_get_value(info->content_ccnb, info->pco->offset[CCN_PCO_E], info->pco, &ptr, &length);
+            printf("Size received asd %d\n", info->pco->offset[CCN_PCO_E]);
+
+            sleep(1);
+
+        }
+
+
         break;
- 
+
     case CCN_UPCALL_INTEREST_TIMED_OUT:
         printf("Interest timed out\n");
         return CCN_UPCALL_RESULT_REEXPRESS;
@@ -76,8 +94,7 @@ enum ccn_upcall_res incoming_interest(struct ccn_closure *selfp,
 
 
 
-void usage(void)
-{
+void usage(void) {
     ///prints the usage and exits
     printf("Usage: %s -t TIME -s REPLY PACKET SIZE(Bytes) [-h] \n\n", CLI_PROGRAM);
     printf("  -t TIME        time interval between consecutive interests, default 1 sec\n");
@@ -86,36 +103,31 @@ void usage(void)
     exit(1);
 }
 
-int main (int argc, char **argv)
-{
-       char *interval_cmd = NULL;
-       double interval;
-       int opt;
-       char *packet_size = NULL;
-       int packet_size_int = 0;
+int main (int argc, char **argv) {
+    char *interval_cmd = NULL;
+    double interval;
+    int opt;
+    char *packet_size = NULL;
+    int packet_size_int = 0;
 
-       
+
     int res = 0;
 
     //check if user supplied uri to trace to, read the arguments and check them
-    if(argc < 3)
-    {
+    if(argc < 3) {
         fprintf(stderr, "%s: Error - Not enough arguments\n\n", CLI_PROGRAM);
         usage();
     }
 
-    while((opt = getopt(argc, argv, "h:t:s:")) != -1)
-    {
-        switch(opt)
-        {
+    while((opt = getopt(argc, argv, "h:t:s:")) != -1) {
+        switch(opt) {
         case 'h':
             usage();
             break;
         case 't':
             interval_cmd = optarg;
             res = sscanf(interval_cmd, "%lf", &interval);
-            if(res == 0)
-            {
+            if(res == 0) {
                 fprintf(stderr, "%s: Error - Could not convert timeout value to int %s\n\n", CLI_PROGRAM, interval_cmd);
                 usage();
             }
@@ -123,13 +135,12 @@ int main (int argc, char **argv)
         case 's':
             packet_size = optarg;
             res = sscanf(packet_size, "%d", &packet_size_int);
-            if(res == 0)
-            {
+            if(res == 0) {
                 fprintf(stderr, "%s: Error - Could not convert packet size to int %s\n\n", CLI_PROGRAM, packet_size);
                 usage();
-            }            
+            }
             break;
-            
+
         case ':':
             fprintf(stderr, "%s: Error - Option `%c' needs a value\n\n", CLI_PROGRAM, optopt);
             usage();
@@ -140,151 +151,123 @@ int main (int argc, char **argv)
             break;
         }
     }
-    
-    #ifdef DEBUG
-     printf("interval %lf secs, packet size in bytes %s\n", interval, packet_size);
-    #endif
-    
+
+#ifdef DEBUG
+    printf("interval %lf secs, packet size in bytes %s\n", interval, packet_size);
+#endif
+
 
 
     //create ccn URI
     // /cbr/1500
-    char *base_uri = "/cbr/";  
-    size_t uri_length = strlen(base_uri) + strlen(packet_size) + 1;
-    
-    char *URI = (char *) malloc(sizeof(char)* uri_length + 1);
-    if(URI == NULL)
-    {
+    char *base_uri = "/cbr";
+    size_t uri_length = strlen(base_uri) + 1 + strlen(packet_size) + 1 + 20 + 1;
+
+    URI = (char *) calloc(uri_length, sizeof(char));
+    if(URI == NULL) {
         fprintf(stderr, "Can not allocate memory for URI\n");
         exit(1);
     }
-    
-    snprintf(URI, uri_length, "%s%s", base_uri, packet_size);
-    
-    #ifdef DEBUG
-        printf("URI is %s\n", URI);
-    #endif
-    
+
+    //append random
+
+    srand ((unsigned int)time (NULL)*getpid());
+    snprintf(URI, uri_length, "%s/%s/%d", base_uri, packet_size, rand());
+
+#ifdef DEBUG
+    printf("URI is %s\n", URI);
+#endif
+
     //allocate memory for interest
     struct ccn_charbuf *ccnb = ccn_charbuf_create();
-    if(ccnb == NULL)
-    {
+    if(ccnb == NULL) {
         fprintf(stderr, "Can not allocate memory for interest\n");
         exit(1);
     }
 
     //adding name to interest
     res = ccn_name_from_uri(ccnb, URI);
-    if(res == -1)
-    {
+    if(res == -1) {
         fprintf(stderr, "Failed to assign name to interest");
         exit(1);
     }
-     
+
 
     //create the ccn handle
     struct ccn *ccn = ccn_create();
-    if(ccn == NULL)
-    {
+    if(ccn == NULL) {
         fprintf(stderr, "Can not create ccn handle\n");
         exit(1);
     }
- 
+
     //connect to ccnd
     res = ccn_connect(ccn, NULL);
-    if (res == -1) 
-    {
+    if (res == -1) {
         fprintf(stderr, "Could not connect to ccnd... exiting\n");
         exit(1);
     }
 
-    #ifdef DEBUG
-        printf("Connected to CCND, return code: %d\n", res);
-    #endif
-    
+#ifdef DEBUG
+    printf("Connected to CCND, return code: %d\n", res);
+#endif
+
 
     struct ccn_closure *incoming;
     incoming = calloc(1, sizeof(*incoming));
     incoming->p = incoming_interest;
-    res = ccn_express_interest(ccn, ccnb, incoming, NULL);
-    if (res == -1)
-    {
-        fprintf(stderr, "Could not express interest for %s\n", URI);
-        exit(1);
-    }
 
-    printf("Expresses interest\n");
+    /*    while(1)
+        {
+        //get time
+
+
+        int milisec = 100; // length of time to sleep, in miliseconds
+        struct timespec req = {0};
+
+
+
+        struct timeval tv;
+        long start_time, end_time, delta;
+
+        gettimeofday(&tv, NULL);
+        start_time = (uint)tv.tv_msec * 1000;   */
+    res = ccn_express_interest(ccn, ccnb, incoming, NULL);
+    //get time
+
+    /* gettimeofday(&tv, NULL);
+     end_time =  (uint)tv.tv_usec * 1000;
+     if (res == -1)
+     {
+         fprintf(stderr, "Could not express interest for %s\n", URI);
+         exit(1);
+     }
+
+     printf("Expresses interest\n");
+
+
+     //calculate delta
+     delta = start_time - end_time;
+     printf("Delta %u\n", delta);
+     req.tv_sec = 0;
+     req.tv_nsec = milisec * 1000000L;
+     nanosleep(&req, (struct timespec *)NULL);
+     nanosleep((struct timespec[]){{0, delta}}, NULL);
+     }*/
 
     //run for timeout miliseconds
     res = ccn_run(ccn, -1);
-    if (res < 0)
-    {
+    if (res < 0) {
         fprintf(stderr, "ccn_run error\n");
         exit(1);
     }
 
+
     //there is a memory leak for incoming, figure a way to free ccn_closure
     ccn_charbuf_destroy(&ccnb);
     ccn_destroy(&ccn);
-    exit(0);           
-        
- return(0);    
- }
+    exit(0);
 
-/*
-    //allocate memory for server URI = /cbr/size/
-    char *URI = (char *) malloc(sizeof(char)* argv_length+1);
-    if(URI == NULL)
-    {
-        fprintf(stderr, "Can not allocate memory for URI\n");
-        exit(1);
-    }
-    
-    //put together the trace URI, add a random number to end of URI
-    srand ((unsigned int)time (NULL)*getpid());
-    sprintf(URI, "%s%s", argv[1]+skip, slash);
+    return(0);
+}
 
 
-    //allocate buffer for response
-    struct ccn_charbuf *resultbuf = ccn_charbuf_create();
-    if(resultbuf == NULL)
-    {
-        fprintf(stderr, "Can not allocate memory for URI\n");
-        exit(1);
-    }
-
-    //setting the parameters for ccn_get
-    struct ccn_parsed_ContentObject pcobuf = { 0 };
-    int timeout_ms = 6000;
-    
-    //express interest
-    res = ccn_get(ccn, ccnb, NULL, timeout_ms, resultbuf, &pcobuf, NULL, 0);
-    if (res == -1)
-    {
-        fprintf(stderr, "Did not receive answer for trace to %s\n", argv[1]);
-	#ifdef DEBUG
-            fprintf(stderr, "Did not receive answer for trace to URI: %s\n", URI);
-        #endif
-        exit(1);
-    }
-
-    //extract data from the response
-    const unsigned char *ptr;
-    size_t length;
-    ptr = resultbuf->buf;
-    length = resultbuf->length;
-    ccn_content_get_value(ptr, length, &pcobuf, &ptr, &length);
-
-    //check if received some data
-    if(length == 0)
-    {    
-        fprintf(stderr, "Received empty answer for trace to %s\n", argv[1]);
-	    #ifdef DEBUG
-            fprintf(stderr, "Received empty answer for trace to URI: %s\n", URI);
-        #endif
-        exit(1);
-    }
-
-    //print the data
-    printf("Reply: %s\n",  ptr);
-    printf("Length of data: %Zu\n", length);*/
--- server.c
+++ server.c
@@ -19,16 +19,16 @@
 #define DEBUG
 
 
-int construct_trace_response(struct ccn *h, struct ccn_charbuf *data, 
-        const unsigned char *interest_msg, const struct ccn_parsed_interest *pi)
-{
+int construct_trace_response(struct ccn *h, struct ccn_charbuf *data,
+                             const unsigned char *interest_msg, const struct ccn_parsed_interest *pi) {
 
     //printf("path:construct trace response");
-    //**this function takes the interest, signs the content and returns to 
+    //**this function takes the interest, signs the content and returns to
     //upcall for further handling
-            
+
     char *mymessage;
-    
+    char *tmp_mymessage;
+
 
     //copy the incoming interest name in ccn charbuf
     struct ccn_charbuf *name = ccn_charbuf_create();
@@ -37,53 +37,50 @@ int construct_trace_response(struct ccn *h, struct ccn_charbuf *data,
     int res;
 
     res = ccn_charbuf_append(name, interest_msg + pi->offset[CCN_PI_B_Name],
-            pi->offset[CCN_PI_E_Name] - pi->offset[CCN_PI_B_Name]);
+                             pi->offset[CCN_PI_E_Name] - pi->offset[CCN_PI_B_Name]);
 
     struct ccn_charbuf *uri = ccn_charbuf_create();
     ccn_uri_append(uri, name->buf, name->length, 1);
 
     char *incoming_uri = ccn_charbuf_as_string(uri);
+
+    //get the random comp, set it to null
+    char *random = strrchr(incoming_uri, '/');
+    *random = '\0';
+
+    //get the size
     char *reply_size_str = strrchr(incoming_uri, '/');
 
-
-
-    if (reply_size_str == NULL)
-    {
-    printf ("Cound not get reply size \n");
-    exit(1);       
-    }
-//    printf("uri = %s reply size %s\n", ccn_charbuf_as_string(uri), reply_size);     */           
-    int reply_size_int;
-    
-    sscanf(reply_size_str+1, "%d", &reply_size_int);
+    //convert str to int
+    int requested_size_int;
+    sscanf(reply_size_str+1, "%d", &requested_size_int);
     int i =0;
-    
-    
-    mymessage = (char *) malloc (sizeof(char) * reply_size_int + 1);
-    //is there a better way?
-    //subtract 378 from
-    for (i = 0; i< reply_size_int; i++)
-    {
+
+
+    mymessage = (char *) malloc (sizeof(char) * requested_size_int + 1);
+
+    //find how many bytes to fill
+    int sign_overhead = 385;
+    int bytes_to_fill = requested_size_int - sign_overhead;
+
+    //if bytes to fill < 0, (if data size asked less than 385 bytes)
+    if (bytes_to_fill < 0) {
+        bytes_to_fill = 1; //send one char
+    }
+
+    for (i = 0; i< bytes_to_fill; i++) {
         mymessage[i] = 'a';
     }
+
     mymessage[i] = '\0';
-            
-    printf("uri = %s Size %d mymessage %d\n", ccn_charbuf_as_string(uri), reply_size_int, strlen(mymessage));     
-    if(res == -1)
-    {
-        fprintf(stderr, "Can not copy interest name to buffer\n");
-        exit(1);
-    }
+    res = ccn_sign_content(h, data, name, &sp, 	mymessage, bytes_to_fill);
+    printf("data length %d %d %d", data->length, bytes_to_fill, strlen(mymessage));
 
-    //sign the content, check if keystore exsists
-    res = ccn_sign_content(h, data, name, &sp, 	mymessage, reply_size_int);
-
-    if(res == -1)
-    {
+    if(res == -1) {
         fprintf(stderr, "Can not sign content\n");
         exit(1);
     }
-    
+
     //free memory and return
     ccn_charbuf_destroy(&sp.template_ccnb);
     ccn_charbuf_destroy(&name);
@@ -93,64 +90,61 @@ int construct_trace_response(struct ccn *h, struct ccn_charbuf *data,
 
 
 enum ccn_upcall_res incoming_interest(struct ccn_closure *selfp,
-        enum ccn_upcall_kind kind, struct ccn_upcall_info *info)
-{
+                                      enum ccn_upcall_kind kind, struct ccn_upcall_info *info) {
 
     //this is the callback function, all interest matching ccnx:/trace
     //will come here, handle them as appropriate
 
-    int res;
+    int res=0;
 
-   
+
     //store the incoming interest name
     struct ccn_charbuf *data = ccn_charbuf_create();
 
-    //check for null, length of incoming interest name   
+    //check for null, length of incoming interest name
 
     //define answer
-   // char *MYMESSAGE="Hello World";
+    // char *MYMESSAGE="Hello World";
 
-    
+
 
     //switch on type of event
-    switch (kind) 
-    {
+    switch (kind) {
     case CCN_UPCALL_FINAL:
-        return CCN_UPCALL_RESULT_OK;    
+        return CCN_UPCALL_RESULT_OK;
         break;
 
-    case CCN_UPCALL_CONTENT:  
+    case CCN_UPCALL_CONTENT:
         //don't care
         break;
 
-    
+
     case CCN_UPCALL_INTEREST:
         //received matching interest
-        //get the interest name from incoming packet      
-        
-        
-        printf("");
-        
+        //get the interest name from incoming packet
+
+
+
         construct_trace_response(info->h, data, info->interest_ccnb, info->pi);
         printf("Sending binary content of length: %Zu \n", data->length);
         res = ccn_put(info->h, data->buf, data->length);
+        if(res < 0)
+            printf("Error sending data\n");
         break;
 
-   default:
-    	break;
+    default:
+        break;
     }
-return(0);
+    return(0);
 }
 
 
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
-   // printf("path:main");
+    // printf("path:main");
     //no argument necessary
-    if(argc != 1)
-    {
+    if(argc != 1) {
         printf("Usage: ./server\n"); // for now, look in flags for local vs remote
         exit(1);
     }
@@ -159,13 +153,12 @@ int main(int argc, char **argv)
 
     //create ccn handle
     struct ccn *ccn = NULL;
-    
+
     //connect to CCN
     ccn = ccn_create();
     //NOTE:check for null
 
-    if (ccn_connect(ccn, NULL) == -1) 
-    {
+    if (ccn_connect(ccn, NULL) == -1) {
         fprintf(stderr, "Could not connect to ccnd");
         exit(1);
     }
@@ -175,8 +168,7 @@ int main(int argc, char **argv)
 
     //We are interested in anythin starting with ccnx:/
     res = ccn_name_from_uri(prefix, "ccnx:/cbr");
-    if (res < 0) 
-    {
+    if (res < 0) {
         fprintf(stderr, "Can not convert name to URI\n");
         exit(1);
     }
@@ -187,9 +179,8 @@ int main(int argc, char **argv)
     in_interest.data = &prefix;
 
     //set the interest filter for prefix we created
-    res = ccn_set_interest_filter(ccn, prefix, &in_interest); 
-    if (res < 0) 
-    {
+    res = ccn_set_interest_filter(ccn, prefix, &in_interest);
+    if (res < 0) {
         fprintf(stderr, "Failed to register interest (res == %d)\n", res);
         exit(1);
     }
